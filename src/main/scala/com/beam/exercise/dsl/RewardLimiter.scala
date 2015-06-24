88db141c998ec22dbe2edba8905dd6774b960b61
dhorne <dhorne@simplemachines.com.au>
2015-06-24 14:21:35 +1000

    Nader after 1.5 hrs

--- src/main/scala/com/beam/exercise/dsl/RewardLimiter.scala
+++ src/main/scala/com/beam/exercise/dsl/RewardLimiter.scala
@@ -1,7 +1,9 @@
 package com.beam.exercise.dsl
 
 import com.beam.exercise.dsl.model.RewardParticipation
+import com.beam.exercise.dsl.model.support.{Completed, Limited}
 import com.beam.exercise.dsl.repository.RewardParticipationRepository
+import com.beam.exercise.dsl.rule.{Limit, ConjunctionLimit, BeamerTotalRewardLimit, TotalRewardBudgetLimit}
 import com.beam.exercise.dsl.service.TimeService
 
 class RewardLimiter(timeService: TimeService, rewardParticipationRepository: RewardParticipationRepository) {
@@ -17,7 +19,44 @@ class RewardLimiter(timeService: TimeService, rewardParticipationRepository: Rew
    * @return the participation with the state set according to the outcome of the limits
    */
   def evaluate(participation: RewardParticipation): RewardParticipation = {
-    ??? // TODO: implement this
+    if(passedConjunctionLimit(participation, participation.reward.limit))
+      participation.copy(status = Completed())
+    else
+      participation.copy(status = Limited())
+
+
+
+   /* participation.reward.limit match {
+      //case BeamerMaximumCyclesLimit(time, count) =>
+      case l: BeamerTotalRewardLimit if l.amount > totalAwardPerBeamer => participation.copy(status = Limited())
+
+      case c: ConjunctionLimit if passedConjunctionLimit (c) => participation.copy(status = Limited())
+
+      case TotalRewardBudgetLimit(totalAward) => participation.copy(status = Limited())
+      case _ => throw new IllegalStateException("it is not")
+    }*/
   }
+
+  private def passedConjunctionLimit(participation: RewardParticipation, conjunctionLimit: Limit): Boolean = {
+    conjunctionLimit match {
+      //case BeamerMaximumCyclesLimit(time, count) =>
+      case l: BeamerTotalRewardLimit => {
+        val totalAwardPerBeamer = rewardParticipationRepository.findByRewardAndBeamerAndStatus(participation.reward, participation.beamer, Completed())
+          .foldLeft(BigDecimal(0))((acc, rewardParticipation) => acc + rewardParticipation.reward.award)
+
+        l.amount < totalAwardPerBeamer
+      }
+      case c: ConjunctionLimit => passedConjunctionLimit(participation,c.left) && passedConjunctionLimit(participation,c.right)
+      case TotalRewardBudgetLimit(totalRewardLimit) => {
+        val totalAward = rewardParticipationRepository.findByRewardAndStatus(participation.reward, Completed())
+          .foldLeft(BigDecimal(0))((acc, rewardParticipation) => acc + rewardParticipation.reward.award)
+
+        totalAward < totalRewardLimit
+      }
+      case _ => throw new IllegalStateException("it is not")
+    }
+
+  }
+
 
 }
